f833b15d2d141c4bfee21d4716acff411057948c
ameyah74 <ameya@m0.ventures>
2024-04-06 23:57:58 +0530

    feat: remove role

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,10 @@
-import { Equals, IsString, Validate } from 'class-validator';
+import { IsString, Validate } from 'class-validator';
 import { IsObjectId } from 'src/_common/decorators/ObjectId.decorator';
 import { IsWalletAddress } from 'src/_common/decorators/walletAddress.decorator';
-import { Role } from 'src/_common/types.global';
 
 export class CreateUserDto {}
 
 export class ConnectWalletDto {
-  @IsString()
-  @Equals(Role.User)
-  role: Role;
-
   @IsString()
   @Validate(IsWalletAddress)
   walletAddress: string;
